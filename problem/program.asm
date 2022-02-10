49ba1d4dc349e8c6ccda051a08fa87d263d97a4d
Val <valeriagonzalezhdez@gmail.com>
2022-02-10 10:17:53 -0500

    Actualizacion de datos

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,6 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// Valeria Gonzalez Hernandez
+// 000147801
+// valeria.gonzalezh@upb.edu.co
 (START)
     @START
     0;JMP
