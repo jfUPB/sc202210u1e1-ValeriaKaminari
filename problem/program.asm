1cf8c1858addf5bd5b263c20f629afb1226a5e40
Val <valeriagonzalezhdez@gmail.com>
2022-02-10 15:02:56 -0500

    actualizacion de datos

--- problem/program.asm
+++ problem/program.asm
@@ -2,43 +2,4 @@
 // 000147801
 // valeria.gonzalezh@upb.edu.co
 (START)
-    @START
-    // Inicializamos la direccion de los input de teclado.
-	@24576
-	D=A
-	@board
-	M=D
-	// Guardamos en memoria codigo de la tecla F.
-	@70
-	D=A
-	@codef
-	M=D
-	// Guardamos en memoria codigo de la tecla C.
-	@67
-	D=A
-	@codec
-	M=D
-(CHECK_BOARD)
-    @fillvalue
-	D=M
-	@blackscreen
-	D;JNE
-    0;JMP
-    // Guardamos en memoria el ultimo pixel a pintar.
-	@24575
-	D=A
-	@current
-	M=D
-	// Leer si se detecta presion en el teclado, y comprobar por el codigo si es una F; consecuente llenar pantalla.
-	@board
-	A=M
-	D=M
-	@fillvalue
-	M=-1
-	@codef
-	D=D-M
-	@DRAW
-	D;JEQ
-	// Si no cumple con la condicion, limpiar pantalla.
-	@fillvalue
-	M=0
+   
