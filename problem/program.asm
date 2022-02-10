d4b2bbbf1a4c7a1fcb68906dd7908feda67cd860
Val <valeriagonzalezhdez@gmail.com>
2022-02-10 10:57:13 -0500

    se guardaron en memoria los codigos de las teclas F y C

--- problem/program.asm
+++ problem/program.asm
@@ -3,4 +3,24 @@
 // valeria.gonzalezh@upb.edu.co
 (START)
     @START
+    // Inicializamos la direccion de los input de teclado.
+	@24576
+	D=A
+	@keyboard
+	M=D
+	// Guardamos en memoria codigo de la tecla F.
+	@70
+	D=A
+	@codef
+	M=D
+	// Guardamos en memoria codigo de la tecla C.
+	@67
+	D=A
+	@codec
+	M=D
+(CHECK_KEYBOARD)
+    @fillvalue
+	D=M
+	@BLACK_SCREEN
+	D;JNE
     0;JMP
