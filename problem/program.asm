019452ae8ec2579635c09107321631231fcf1985
Val <valeriagonzalezhdez@gmail.com>
2022-02-11 15:22:46 -0500

    Comprobar el funcionamiento de las teclas

--- problem/program.asm
+++ problem/program.asm
@@ -2,4 +2,33 @@
 // 000147801
 // valeria.gonzalezh@upb.edu.co
 (START)
-   
+	@70
+	D=A
+	@2
+	M=D
+    @67
+	D=A
+	@3
+	M=D
+	@24576
+	D=A
+    @1
+	M=D
+    @4
+	D=M
+	@46
+	D;JNE
+	@24575
+	D=A
+	@20
+	M=D
+	@1
+	A=M
+	D=M
+	@4
+	M=-1
+	@2
+	D=D-M
+	@31
+	D;JEQ
+	
